c0f60671d06b6228fa06f5e2eddd96cbe7f54d0f
Rob B <rvabcode@gmail.com>
2021-11-24 11:36:00 -0500

    added conditionals

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// operators
+// Operators
 
 const oppo = 3 > 2;
 console.log(oppo); //this returns true
@@ -12,7 +12,7 @@ console.log(oppo3); //this returns false
 const oppo4 = 3 <= 2;
 console.log(oppo4); //this returns false
 
-// strict operators
+// Strict operators
 
 const oppo5 = 3 !== 2;
 console.log(oppo5); //this returns true
@@ -22,3 +22,25 @@ console.log(oppo6); //this returns false
 
 const oppo7 = null === undefined;
 console.log(oppo7); //this returns false
+
+const oppo8 = 5 === '5';
+console.log(oppo7); //this returns false
+
+// Conditionals
+// if statement, keep in mind what is in the parenthesis is a condition, you are writing a condition inside of it.
+// When I just write this condition of true, it is saying it is always true for this if statement.
+
+if (true) {
+}
+
+// Here I can write an expression inside the parenthesis
+if (1 > 0) {
+  console.log('Yes, 1 is greater than 0');
+}
+
+// I can also put a variable as my condition
+let somethingTrue = true;
+
+if (somethingTrue === true) {
+  console.log('I am true');
+}
