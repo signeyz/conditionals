6cfade7f8e6fcd66bb07aa2c933a292ed047bbfc
Rob B <rvabcode@gmail.com>
2021-11-24 11:23:23 -0500

    added operators and strict operators

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+// operators
 
+const oppo = 3 > 2;
+console.log(oppo); //this returns true
+
+const oppo2 = 3 >= 2;
+console.log(oppo2); //this returns true
+
+const oppo3 = 3 < 2;
+console.log(oppo3); //this returns false
+
+const oppo4 = 3 <= 2;
+console.log(oppo4); //this returns false
+
+// strict operators
+
+const oppo5 = 3 !== 2;
+console.log(oppo5); //this returns true
+
+const oppo6 = 3 === 2;
+console.log(oppo6); //this returns false
+
+const oppo7 = null === undefined;
+console.log(oppo7); //this returns false
